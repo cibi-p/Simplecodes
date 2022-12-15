ad85eefd76161f151b365b01affe315f5a0ecaf6
broharigunda <brohari740@gmail.com>
2022-12-15 17:44:41 +0530

    added time consuming

--- Programs/dynamic_array.c
+++ Programs/dynamic_array.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<time.h>
 int* create_array(int size)
 {
   int *a = (int *) malloc(size*sizeof(int));
@@ -10,6 +10,8 @@ int* create_array(int size)
 
 int main()
 {
+    clock_t t;
+    t=clock();
     int m_s_a=5,s=0;
     int *a=create_array(m_s_a);
     for(int i=0;i<10;i++){
@@ -35,5 +37,7 @@ int main()
     {
       printf("%d ",a[i]);
     }
+    t=clock()-t;
+    printf("\n%f", ((double)t)/CLOCKS_PER_SEC);
     return 0;
 }
